c812732a6e5924391fa1b2ab892099cd19031e17
Shimanti <shimanti@gmail.com>
2022-03-21 10:35:27 -0700

    added comments and reset year

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const specialDate = new Date("25 Dec 2022")
-// const pomodoro = Date.now() + 25 * 60 * 1000
+// get today for current year
+const today = new Date()
 
+// set Xmas to current year
+let xmas = new Date(today.getFullYear(), 11, 25)
+
+// reset year if it's after Xmas Day
+if(today > xmas) {
+  xmas = new Date(today.getFullYear()+1,11,25) 
+}
+
+// create countdown timer
 const countdown = () => {
   const current = new Date()
-  const totalSeconds = (specialDate - current) / 1000
+  const totalSeconds = (xmas - current) / 1000
   
   const days = Math.floor(totalSeconds / 60 / 60 / 24)
   const hours = Math.floor(totalSeconds / 60 / 60 ) % 24
@@ -16,7 +25,11 @@ const countdown = () => {
   document.querySelector('#seconds').innerHTML = format(seconds)
 }
 
+// format time for less than 10
 const format = (time) => time < 10 ? `0${time}`: time
 
+// initiate timer
 countdown()
+
+// countdown seconds
 setInterval(countdown, 1000)
